d83a8b07787bdc73bcfac915dab0ab2d9567350b
Amiel Antonio <amiel@staffink.com.au>
2024-05-31 14:14:33 +0800

    Additional logging todos

---
+++ classes/inc/class-logger.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace GFAgentbox\Inc;
+
+/**
+ * Class that creates own log file
+ */
+class StafflinkLogger
+{
+    /**
+     * File path of the log file
+     *
+     * @var string
+     */
+    protected $log_file;
+
+    /**
+     * File name of the log file
+     *
+     * @var string
+     */
+    protected $file_name;
+
+    /**
+     * Levels of logging information
+     *
+     * @var array
+     */
+    protected $log_level = [ 
+        'DEBUG' => 0,
+        'INFO'  => 1,
+        'ERROR' => 2,
+    ];
+
+    /**
+     * Class Consturctor
+     *
+     * @param string $file_path
+     * @param string $file_name
+     */
+    public function __construct( $file_path = "", $file_name = "sl_debug" )
+    {
+        $this->file_name = $file_name;
+        // create the log file
+        $this->log_file = $this->set_log_file( $file_path );
+    }
+
+    /**
+     * Set the log file and path
+     *
+     * @param string $file_path
+     * @return string
+     */
+    public function set_log_file( $file_path )
+    {
+        return ( $file_path == "" ) ? WP_CONTENT_DIR . "/{$this->file_name}.log" : $file_path;
+    }
+
+
+    /**
+     * Create a log entry to the log file
+     *
+     * @param string $message
+     * @return void
+     */
+    public function log( $message )
+    {
+        $message = $this->create_message( $message );
+
+        file_put_contents( $this->log_file, $message, FILE_APPEND );
+    }
+
+    public function log_info()
+    {
+
+    }
+
+    public function log_debug()
+    {
+
+    }
+
+    public function log_error()
+    {
+
+    }
+
+    /**
+     * Formats the message for the log file
+     *
+     * @param string $message
+     * @return string
+     */
+    public function create_message( $message ): string
+    {
+        $timestamp = date( 'Y-m-d H:i:s' );
+        $message   = "[{$timestamp}] {$message}" . PHP_EOL;
+
+        return $message;
+    }
+
+    public function delete_log_file()
+    {
+
+    }
+
+    public function get_log_dir()
+    {
+
+    }
+
+    public function get_log_file_name()
+    {
+
+    }
+}
